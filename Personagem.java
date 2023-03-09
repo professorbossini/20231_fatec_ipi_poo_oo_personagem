ce248ed5bbe7d3e7b958b3f1330cdd9e33e428fa
Rodrigo Bossini <professorbossini@gmail.com>
2023-03-09 17:55:00 -0300

    apostila02(oo): bloco de codigo 2.5.1 encerrado

--- Personagem.java
+++ Personagem.java
@@ -2,9 +2,21 @@ import static java.lang.Math.min;
 public class Personagem{
   //variável de instância
   String nome;
-  int energia = 10;
-  int fome = 0;
-  int sono = 0;
+  private int energia = 10;
+  private int fome = 0;
+  private int sono = 0;
+
+  //construtor padrão: lista de parâmetros vazia
+  Personagem(int energia, int fome, int sono){
+    //shadowing (sombreamento)
+    if (energia >= 0 && energia <= 10)
+      this.energia = energia;
+    if (fome >= 0 && fome <= 10)
+      this.fome = fome;
+    if (sono >= 0 && sono <= 10)
+      this.sono = sono;
+    System.out.println("Construindo um objeto...");
+  }
 
   //isso é um método de instância
   void cacar(){
@@ -18,6 +30,7 @@ public class Personagem{
     }
     fome = min(fome + 1, 10);
     sono = min(sono + 1, 10);
+    System.out.println(obterEstado());
   }
 
   void comer(){
@@ -29,6 +42,7 @@ public class Personagem{
     else{
       System.out.println(nome + " sem fome");
     }
+    System.out.println(obterEstado());
   }
 
   void dormir(){
@@ -39,6 +53,11 @@ public class Personagem{
     }
     else 
       System.out.println(nome + " sem sono");
+    System.out.println(obterEstado());
+  }
+
+  String obterEstado(){
+    return String.format("e(%d), s(%d), f(%d)", energia, sono, fome);
   }
 
 }
--- TestePersonagem.java
+++ TestePersonagem.java
@@ -1,13 +1,35 @@
 public class TestePersonagem{
-  static public void main(String [] args){
+  static public void main(String [] args) throws Exception{
+
     //p é uma variável de referência. Ela faz referência ao objeto construído com new
     // Personagem p = new Personagem();
     //java 10+ (inferência de tipo)
-    var p = new Personagem();
+    var p = new Personagem(2, 4, 10);
+    var p2 = new Personagem(8, 10, 2);
     p.nome = "John";
-    p.cacar();
-    p.comer();
-    p.dormir();
+    while (true){
+      p.cacar();
+      Thread.sleep(5000);
+      p.comer();
+      Thread.sleep(5000);
+      p.dormir();
+      Thread.sleep(5000);
+      p.cacar();
+      Thread.sleep(5000);
+      p.cacar();
+      Thread.sleep(5000);
+      p.cacar();
+      System.out.println("======================================");
+      Thread.sleep(10000);
+    }
 
   }
 }
+    // int idade = 18;
+
+    // if (idade >= 18)
+    //   System.out.println("Sim, pode dirigir");
+    // else
+    //   System.out.println("Não, não pode dirigir");
+
+    // System.out.println(idade >= 18 ? "Sim" : "Não");
